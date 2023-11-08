a79690f05db7abc5b5824c732e2747c829664192
chandiosamad07 <absamadchandio@gmail.com>
2023-11-08 17:37:57 +0500

    CSR and SSR

--- next-app/app/page.js
+++ next-app/app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
    <>
    <h1>Hello samad</h1>
    <Link href={'/users'}>users</Link>
+   <ProductCard />
    </>
   )
 }
