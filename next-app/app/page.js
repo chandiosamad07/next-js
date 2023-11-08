494fcc215aebcff89c9d32ae4e59ea3eed90c115
chandiosamad07 <absamadchandio@gmail.com>
2023-11-08 11:47:24 +0500

    Navigation using Link

--- next-app/app/page.js
+++ next-app/app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
    <>
    <h1>Hello samad</h1>
+   <Link href={'/users'}>users</Link>
    </>
   )
 }
